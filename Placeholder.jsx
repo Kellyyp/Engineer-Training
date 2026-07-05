dc0d4b1f5e4d8290c5833c415cf206178f024f71
Kellyyp <k.piscopo1@yahoo.com>
2026-07-05 19:45:57 -0400

    Delete Placeholder.jsx

--- Placeholder.jsx
+++
@@ -1,9 +0,0 @@
-export default function Placeholder({ title, text }) {
-  return (
-    <section className="placeholder">
-      <p className="eyebrow">Coming Soon</p>
-      <h1>{title}</h1>
-      <p>{text}</p>
-    </section>
-  )
-}
